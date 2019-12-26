af09fc95c74ed261af52a3e03f5485d94bf0a0bf
torvarun <torvarun@gmail.com>
2019-12-26 12:45:40 -0500

    displying scoring combos with min 3 goals

--- goals.R
+++ goals.R
@@ -12,10 +12,6 @@ goals_all <- subset(goals_raw, select=c(index, date, scorer, primary_assist, sec
 # 
 # scoring <- merge(primary, secondary, by="player")
 # colnames(scoring) <- c("player", "primary", "secondary")
-# scoring <- data.frame(append(scoring, c("goals"=0), after = 1), stringsAsFactors=FALSE)
-# remove(primary, secondary)
-# 
-# scoring[nrow(scoring)+1,] <- list(player="Alexander Ovechkin", goals=nrow(goals_all), primary=0, secondary=0)
 
 # Prerpare the data for an alluvial chart
 scoring <- dplyr::count(goals_all, scorer, primary_assist, secondary_assist)
@@ -27,11 +23,10 @@ presenter <- scoring[which(scoring$n >= 3), names(scoring) %in% c("scorer", "pri
 ggplot(as.data.frame(presenter),
        aes(axis1 = primary_assist, axis2 = secondary_assist)) +
   geom_alluvium(aes(fill=primary_assist), width = 0, knot.pos = 0) +
-  geom_stratum(width = 1/12, fill = "black", color = "grey") +
+  geom_stratum(width = 1/50, fill = "black", color = "grey") +
   geom_label(stat = "stratum", infer.label = TRUE) +
   scale_x_discrete(limits = c("Primary Assist", "Secondary Assist"), expand = c(.05, .05)) +
-  #scale_fill_brewer(type = "qual", palette = "Set1") +
   ggtitle("Ovechkin's Goals: Scoring Combinations (min 3)") +
   theme(legend.position = "none")
 
-        
+remove(goals_all, goals_raw, presenter, scoring)
